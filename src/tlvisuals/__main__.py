c34328cb88446173bf38289a8a43045bc351a701
razifp <muhammadrazif.pramuda@idemia.com>
2024-05-10 14:20:23 +0700

    add subcommand to convert ascii hex to raw

--- src/tlvisuals/__main__.py
+++ src/tlvisuals/__main__.py
@@ -5,19 +5,40 @@ import json
 from tlvisuals.tlv_parser import ByteGetter, TLVParser, DiagnosticsCollector, DerByteGetter
 from tlvisuals.output_builder.raw_format import RawFormatBuilder
 
+
 def main():
    parser = argparse.ArgumentParser(
-      prog='TLVisuals',
-      description='Prints TLV in a readable format. \
-      Without options, app will read from standard input and assume the format is Hex in ASCII form, and output will be to standard output',
+      prog="tlvisuals",
+      description="Utilities for parsing and printing TLV in a more user friendly format",
       formatter_class=RawTextHelpFormatter
       )
-   parser.add_argument('-f', '--file', help="Parses input from specified FILE")
-   parser.add_argument('-o', '--out', help="Writes output to specified OUT file")
    parser.add_argument('-v', '--verbose',action='store_true', help="Outputs more logs")  # on/off flag
-   parser.add_argument('--input-format', help="Specifies input format:\n -der: input is raw hex\notherwise assumed to be ascii hex")  # inform formats: 'der', 'hextext'
-   parser.add_argument('--output-format', help="Specifies output format:\n -interpretation: shows basic TLV flag interpretation")
+   subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
+
+   tlvparse_parser = subparsers.add_parser(name="tlvparse", description="Without options, app will read from standard input and assume the format is Hex in ASCII form, and output will be to standard output")
+   tlvparse_parser.add_argument('-f', '--file', help="Parses input from specified FILE")
+   tlvparse_parser.add_argument('-o', '--out', help="Writes output to specified OUT file")
+   tlvparse_parser.add_argument('--input-format', help="Specifies input format:\n -der: input is raw hex\notherwise assumed to be ascii hex")  # inform formats: 'der', 'hextext'
+   tlvparse_parser.add_argument('--output-format', help="Specifies output format:\n -interpretation: shows basic TLV flag interpretation")
+
+   hextoraw_parser = subparsers.add_parser(name="hextoraw",description="Converts ASCII hex input to raw bytes, ignoring whitespaces")
+   hextoraw_parser.add_argument('-f', '--file', help="Reads input from specified FILE")
+   hextoraw_parser.add_argument('-o', '--out', help="Writes output to specified OUT file")
    args = parser.parse_args()
+
+   
+   match(args.subcommand):
+      case "tlvparse":
+         sys.exit(tlvparse(args=args))
+      case "hextoraw":
+         sys.exit(hextoraw(args=args))
+      case _:
+         print("Unknown command")
+         parser.print_help()
+         sys.exit(1)
+   
+
+def tlvparse(args):
    # print(args.file, args.out, args.verbose)
 
    # construct byte getter
@@ -53,6 +74,24 @@ def main():
    else:
       sys.stdout.write(out_str)
    
+   return 0
+
+
+def hextoraw(args):
+   # construct byte getter
+   if args.file:
+      input_stream = open(args.file, 'r')
+   else:
+      input_stream = sys.stdin
+   byte_getter = ByteGetter(input_stream)
+   out_bytes = bytes(byte_getter)
+    # write to stream
+   if args.out:
+      with open(args.out, 'wtb') as f:
+         f.write(out_bytes)
+   else:
+      sys.stdout.buffer.write(out_bytes)
+
    return 0
 
 
